8398dda0c6b142642f701d48f69c06bd7a3e038f
balaramkrishna16 <bbrk4353@gmail.com>
2026-02-17 10:51:43 +0530

    17/02/26 commit

--- demo/src/com/demo/TestDemo1.java
+++ demo/src/com/demo/TestDemo1.java
@@ -1,9 +1,26 @@
 package com.demo;
+import java.math.BigInteger;
+import java.math.BigDecimal;
+
 
 public class TestDemo1 {
 
 	public static void main(String[] args) {
-		System.out.println("hello world !");
+		BigInteger bi1=new BigInteger("123456789123456789");
+		BigInteger bi2=new BigInteger("987654321987654321");
+		BigInteger sum = bi1.add(bi2); 
+		BigInteger difference = bi1.subtract(bi2); 
+		BigInteger product = bi1.multiply(bi2); 
+		BigInteger quotient = bi1.divide(bi2); 
+		BigInteger remainder = bi1.remainder(bi2); 
+
+
+		System.out.println("Sum: " + sum);
+		System.out.println("Difference: " + difference);
+		System.out.println("Product: " + product);
+		System.out.println("Quotient: " + quotient);
+		System.out.println("Remainder: " + remainder);
+		
 
 	}
 
